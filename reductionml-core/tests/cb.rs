95b127b393310748d4aa6d96799ddd8ec627b46b
Alexey Taymanov <ataymano@gmail.com>
2023-07-08 01:37:14 -0400

    first e2e cb test

--- reductionml-core/tests/cb.rs
+++ reductionml-core/tests/cb.rs
@@ -6,8 +6,9 @@ use reductionml_core::{
     object_pool::Pool,
     parsers::{JsonParserFactory, TextModeParser, TextModeParserFactory},
     reduction::DepthInfo,
+    reduction::ReductionWrapper,
     reduction_factory::ReductionFactory,
-    reductions::{CBExploreAdfGreedyConfig, CBExploreAdfGreedyReductionFactory},
+    reductions::{CBExploreAdfGreedyConfig, CBExploreAdfGreedyReductionFactory, CBExploreAdfSquareCBConfig, CBExploreAdfSquareCBReductionFactory},
     sparse_namespaced_features::{Namespace, SparseFeatures},
     utils::AsInner,
     ActionProbsPrediction, CBAdfFeatures, CBLabel, Features, FeaturesType, Label, LabelType,
@@ -119,4 +120,154 @@ fn test_greedy_predict_json() {
     );
     let pred: &ActionProbsPrediction = prediction.as_inner().unwrap();
     assert!(pred.0.len() == 2);
+}
+
+fn ex_learn(
+    context: &str,
+    action0: &str,
+    action1: &str,
+    chosen: i32,
+    p: f32,
+    reward: f32) -> String {
+        json!({
+            "label": {
+                "action": chosen,
+                "cost": -reward,
+                "probability": p
+            },
+            "shared": {
+                "shared": {
+                    "f": context,
+                }
+            },
+            "actions": [
+                {
+                    "action": {
+                        "f": action0
+                    }
+                },
+                {
+                    "action": {
+                        "f": action1
+                    }
+                }
+            ]
+        }).to_string()
+}
+
+fn ex_pred(
+    context: &str,
+    action0: &str,
+    action1: &str
+    ) -> String {
+        json!({
+            "shared": {
+                "shared": {
+                    "f": context,
+                }
+            },
+            "actions": [
+                {
+                    "action": {
+                        "f": action0
+                    }
+                },
+                {
+                    "action": {
+                        "f": action1
+                    }
+                }
+            ]
+        }).to_string()
+}
+
+
+fn test_learning_e2e(
+    context: fn(i32) -> String,
+    action0: &str,
+    action1: &str,
+    chosen: fn(&str, i32) -> (i32, f32),
+    r: fn(&str, &str) -> f32,
+    n: i32,
+    learners: &mut [ReductionWrapper],
+    global_config: &GlobalConfig,
+    test_set: &[(String, usize)]
+) {
+    for mut learner in learners.iter_mut() {
+        let pool = Arc::new(Pool::new());
+        let json_parser_factory = JsonParserFactory::default();
+        let json_parser = json_parser_factory.create(
+            FeaturesType::SparseCBAdf,
+            LabelType::CB,
+            0,
+            global_config.num_bits(),
+            pool.clone(),
+        );
+        let actions = [action0, action1];
+        for i in 0..n {
+            let ctx = context(i);
+            let (chosen_action, p) = chosen(&ctx, i);
+            let reward = r(&ctx, &actions[usize::try_from(chosen_action).unwrap()]);
+            let (mut features, label) = json_parser.parse_chunk(
+                &ex_learn(&ctx, action0, action1, chosen_action, p, reward)).unwrap();
+
+            let mut depth_info = DepthInfo::new();
+            learner.learn(
+                &mut features,
+                &label.unwrap(),
+                &mut depth_info,
+                0.into(),
+            );
+        }
+        for (ctx, expected) in test_set {
+            let (mut features, _) = json_parser.parse_chunk(
+                &ex_pred(&ctx, action0, action1)).unwrap();
+            let mut depth_info = DepthInfo::new();
+            let prediction = learner.predict(
+                &mut features,
+                &mut depth_info,
+                0.into(),
+            );
+            let pred: &ActionProbsPrediction = prediction.as_inner().unwrap();
+            assert!(0==0);
+            for (action, prob) in pred.0.iter() {
+                if action.eq(expected) {
+                    assert!(prob > &0.5)
+                }
+            }
+        }              
+    }
+}
+
+
+#[test]
+fn test_learning_const() {
+    fn context(i: i32) -> String {
+        "Tom".to_string()
+    }
+    fn chosen(context: &str, i: i32) -> (i32, f32) {
+        (i % 2, 0.5)
+    }
+    fn r(context: &str, action: &str) -> f32 {
+        if action == "Politics" { 1.0 } else { 0.0 }
+    }
+
+    let global_config = GlobalConfig::new(4, 0, true, &Vec::new());
+    let factory = CBExploreAdfGreedyReductionFactory::default();
+    let mut learners = [
+        CBExploreAdfGreedyReductionFactory::default().create(&CBExploreAdfGreedyConfig::default(), &global_config, 1.into()).unwrap(),
+ //       CBExploreAdfSquareCBReductionFactory::default().create(&CBExploreAdfSquareCBConfig::default(), &global_config, 1.into()).unwrap(),
+    ];
+
+    test_learning_e2e(
+        context,
+        "Politics",
+        "Sports",
+        chosen,
+        r,
+        10000,
+        &mut learners,
+        &global_config,
+        &[("Tom".to_owned(), 0)]
+        );
 }
