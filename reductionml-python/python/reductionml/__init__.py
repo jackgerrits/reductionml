893ba175edce406f9736cfc3e53f8a60dddc2fd5
Jack Gerrits <jackgerrits@users.noreply.github.com>
2023-07-10 19:51:39 -0400

    docs: fix python docs

--- reductionml-python/python/reductionml/__init__.py
+++ reductionml-python/python/reductionml/__init__.py
@@ -1,4 +1,9 @@
 from ._reductionml import *
 from . import _reductionml
+import typing
 
 __doc__ = _reductionml.__doc__
+
+if not typing.TYPE_CHECKING:
+    if hasattr(_reductionml, "__all__"):
+        __all__ = _reductionml.__all__
