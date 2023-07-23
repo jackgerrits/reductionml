jackgerrits/reductionml

commit b10c4b163ff35fe44b972fb1770393ed3828438c

feat: reduction property `config` can be omitted in config json file when empty (#63)

reductionml-core/src/reduction_factory.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 use schemars::schema::RootSchema;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
 
 use crate::{
     error::{Error, Result},
@@ -16,11 +16,47 @@ use crate::{
 
 // This intentionally does not derive JsonSchema
 // Use gen_json_reduction_config_schema instead with schema_with
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Clone, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct JsonReductionConfig {
     typename: PascalCaseString,
+    // TODO: ensure json schema reflects that this is option based on the below deserialize impl
     config: serde_json::Value,
+}
+
+impl<'de> Deserialize<'de> for JsonReductionConfig {
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let mut json: serde_json::value::Value =
+            serde_json::value::Value::deserialize(deserializer)?;
+        let typename = json
+            .get("typename")
+            .expect("type")
+            .as_str()
+            .unwrap()
+            .to_string()
+            .try_into()
+            .map_err(|x| serde::de::Error::custom(x))?;
+        let config = json.get_mut("value");
+
+        match config {
+            Some(config) => Ok(JsonReductionConfig::new(typename, config.take())),
+            None => {
+                let default_config = REDUCTION_REGISTRY
+                    .read()
+                    .as_ref()
+                    .unwrap()
+                    .get(&typename.0)
+                    .ok_or_else(|| {
+                        serde::de::Error::custom(format!("Unknown reduction type: {}", typename))
+                    })?
+                    .get_config_default();
+                Ok(JsonReductionConfig::new(typename, default_config))
+            }
+        }
+    }
 }
 
 impl JsonReductionConfig {
@@ -201,5 +237,29 @@ pub fn create_reduction(
             "Unknown reduction type: {}",
             config.typename()
         ))),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use crate::reduction_factory::JsonReductionConfig;
+
+    #[test]
+    fn parse_json_config_with_no_config() {
+        let _config: JsonReductionConfig = serde_json::from_value(json!( {
+            "typename": "Coin"
+        }))
+        .unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn parse_json_config_with_no_config_does_not_exist() {
+        let _config: JsonReductionConfig = serde_json::from_value(json!( {
+            "typename": "DoesNotExist",
+        }))
+        .unwrap();
     }
 }

commit 64d5eeb65ce308d8a9e770d5407e3ce4d65dff86

fix: Fix bug which was forcing default configs always
Introduced in #63

reductionml-core/src/reduction_factory.rs
@@ -39,7 +39,7 @@ impl<'de> Deserialize<'de> for JsonReductionConfig {
             .to_string()
             .try_into()
             .map_err(|x| serde::de::Error::custom(x))?;
-        let config = json.get_mut("value");
+        let config = json.get_mut("config");
 
         match config {
             Some(config) => Ok(JsonReductionConfig::new(typename, config.take())),